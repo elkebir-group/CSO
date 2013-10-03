9d3991d58fcd9a35447cc32cb4c8a03fbce575a8
inidaster <inidaster@gmail.com>
2013-10-03 15:21:35 +0200

    Correct seed in every .xml

--- scripts/generate.py
+++ scripts/generate.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 import sys
 import subprocess
+import random
+import time
 
 if len(sys.argv) != 7:
 	print "Usage: " + sys.argv[0] + " <csogenerator> r m n p a"
@@ -13,14 +15,25 @@ list_n = map(int, sys.argv[4].split(","))
 list_p = map(float, sys.argv[5].split(","))
 list_a = map(int, sys.argv[6].split(","))
 
+old_seed = 0
 for m in list_m:
 	for n in list_n:
 		for p in list_p:
 			for a in list_a:
 				if a <= m:
-					for i in range(r):
-						print m, n, p, a, i
-						command = csogenerator + " -m "+str(m) + " -n "+str(n) +" -p "+ str(p) +" -a "+ str(a)
-						f = open("m_" + str(m) + "-n_" + str(n) + "-p_" + str(p) + "-a_" + str(a)+ "-i_" + str(i) + ".xml", "w")
-						subprocess.call(command, shell=True, stdout=f)
-						f.close()
+					for i in range (r):
+					    #seed = int(time.time())
+					    #r1 = random.Random(seed)
+					    #actual_random = r1.random()
+					    #actual_random =int(actual_random)
+					    start = time.clock()
+					    seed = random.randint(0,1000000)
+					    if seed == old_seed:
+						seed = random.randint(0,1000000)
+						print "Found same seed. New seed is:", seed
+					    print m, n, p, a, i, seed
+					    command = csogenerator + " -s " + str(seed) + " -m "+ str(m) + " -n "+ str(n) +" -p "+ str(p) +" -a "+ str(a)
+					    f = open("-s_" + str(seed) + "-m_" + str(m) + "-n_" + str(n) + "-p_" + str(p) + "-a_" + str(a)+ "-i_" + str(i) + ".xml", "w")
+					    subprocess.call(command, shell=True, stdout=f)
+					    old_seed = seed
+					    f.close()
